f4f5988681bef967f602f2cccace1658052a8f61
Chirayu Desai <chirayudesai1@gmail.com>
2022-09-17 04:47:50 +0530

    Enable GRALLOC_HANDLE_HAS_RESERVED_SIZE
    * Need to make camera work on 13
    
    Change-Id: I0d78f44386129b1fad30bec6ef82000df130d631

--- device-common.mk
+++ device-common.mk
@@ -760,6 +760,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
     frameworks/native/data/etc/android.hardware.device_unique_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.device_unique_attestation.xml
 
+# Needed for camera
+TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
+
 # RCS
 PRODUCT_PACKAGES += \
     PresencePolling \
